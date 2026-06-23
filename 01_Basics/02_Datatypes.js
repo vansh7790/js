7cf69721aa1bf4cf7db75a9d08367899eaf2e203
Vansh Verma <vvansh0011@gmail.com>
2026-06-23 16:29:33 +0530

    Add initial data types example in 02_Datatypes.js

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //Treats all js code as newer version of js.//
+let name="John Doe"//String
+let age=30//Number
+let isEmployed=true//Boolean
+let hobbies=["reading","traveling","coding"]//Array
+let address={street:"123 Main St",city:"New York",zip:"10001"}//Object
+console.log(typeof name); //string
+console.log(typeof age); //number
+console.log(typeof isEmployed); //boolean
+console.log(typeof null); //object (this is a known quirk in JavaScript)    
+console.log(typeof undefined); //undefined
+
